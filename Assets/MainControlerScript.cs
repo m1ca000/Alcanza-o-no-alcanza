07cd7a377f198ddea5eab5c0d840b5ae24732cf4
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-06-25 14:29:19 -0300

    Producto Script y Main Controler

---
+++ Assets/MainControlerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControlerScript : MonoBehaviour
+{
+    public GameObject[] productos;
+    public int currentIndex = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DeactivateAll()
+    {
+        for (int i = 0; i < productos.Length; i++)
+        {
+            productos[i].SetActive(false);
+        }
+    }
+}
