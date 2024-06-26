a7428a48e459b6e9e48639f815062ecbb78767e4
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-06-26 08:19:15 -0300

    Se muestra precio del producto

--- Assets/MainControlerScript.cs
+++ Assets/MainControlerScript.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainControlerScript : MonoBehaviour
 {
     public GameObject[] productos1;
     public GameObject[] productos2;
     public int currentIndex = -1;
+    public Text precio1;
+    public Text precio2;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,13 +40,14 @@ public class MainControlerScript : MonoBehaviour
     {
         int random = Random.Range(0, productos1.Length);
         productos1[random].SetActive(true);
-
+        ProductoScript producto1;
+        producto1 = productos1[random].GetComponent<ProductoScript>();
+        precio1.text = producto1.precio.ToString();
+        
         int random2 = Random.Range(0, productos2.Length);
         productos2[random2].SetActive(true);
-    }
-
-    void MostrarPrecio() 
-    {
-
+        ProductoScript producto2;
+        producto2 = productos2[random2].GetComponent<ProductoScript>();
+        precio2.text = producto2.precio.ToString();
     }
 }
